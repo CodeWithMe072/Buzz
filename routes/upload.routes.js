3bf1cc3fd30313c40ab9061837dce9eaad92d2d7
Sanjay chouhan <sanjaystar14581@gmail.com>
2026-02-16 23:00:34 +0530

    complate audio system

--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -7,12 +7,18 @@ const router = express.Router();
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: async (req, file) => ({
-    folder: "chat_media",
-    resource_type: file.mimetype.startsWith("video") ? "video" : "image",
-    public_id: `${Date.now()}_${file.originalname.split(".")[0]}`
-  })
+  params: async (req, file) => {
+    const isVideo = file.mimetype.startsWith("video/");
+    const isAudio = file.mimetype.startsWith("audio/");
+
+    return {
+      folder: "chat_media",
+      resource_type: (isVideo || isAudio) ? "video" : "image",
+      public_id: `${Date.now()}_${file.originalname.split(".")[0]}`
+    };
+  }
 });
+
 
 
 const upload = multer({
@@ -28,31 +34,40 @@ router.post("/api/upload", upload.single("file"), (req, res) => {
     return res.status(400).json({ error: "No file uploaded" });
   }
 
-  const isVideo = req.file.mimetype.startsWith("video") ? true : false
-  const publicId = req.file.filename; // Cloudinary public_id
-  const baseUrl = req.file.path.split("/upload/")[0] + "/upload";
-  let response;
+  const mime = req.file.mimetype;
 
+  const isVideo = mime.startsWith("video/");
+  const isAudio = mime.startsWith("audio/");
+  const publicId = req.file.filename;
+  const baseUrl = req.file.path.split("/upload/")[0] + "/upload";
+  console.log(req.file.path)
   if (isVideo) {
-    response = {
+    return res.status(200).json({
       type: "video",
       original: req.file.path,
-
       cover_270: `${baseUrl}/so_0,w_270,h_270,c_fill,f_jpg/${publicId}.jpg`,
       thumb_50: `${baseUrl}/so_0,w_50,h_50,c_fill,f_jpg/${publicId}.jpg`
-    };
-  } else {
-    response = {
-      type: "image",
-      original: req.file.path,
-
-      cover_270: `${baseUrl}/w_270,h_270,c_fill/${publicId}.jpg`,
-      thumb_50: `${baseUrl}/w_50,h_50,c_fill/${publicId}.jpg`
-    };
+    });
   }
 
-  res.status(200).json(response);
+  if (isAudio) {
+    return res.status(200).json({
+      type: "audio",
+      original: req.file.path,
+      cover_270: null,
+      thumb_50: null
+    });
+  }
+
+  // image
+  return res.status(200).json({
+    type: "image",
+    original: req.file.path,
+    cover_270: `${baseUrl}/w_270,h_270,c_fill/${publicId}.jpg`,
+    thumb_50: `${baseUrl}/w_50,h_50,c_fill/${publicId}.jpg`
+  });
 });
+
 
 
 export default router;
